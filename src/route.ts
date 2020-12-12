40038a2b38c639df9106ddf612443d446b2ae3ae
Guilherme Isaías <guilherme@localhost.localdomain>
2020-12-12 18:11:28 -0300

    Método para registrar as rotas no servidor

--- src/route.ts
+++ src/route.ts
@@ -1,4 +1,5 @@
 import http from 'http';
+import Server from './server';
 
 export default class Route {
     private _path: string;
@@ -11,9 +12,14 @@ export default class Route {
         this._method = method;
     }
 
-    public assignInServer(server: http.Server): http.Server {
-        server.on(this._method, this._view);
+    public assignInServer(server: Server): Server {
+        server.server.on(this._method, this._view);
         return server;
     }
 
+}
+
+
+export const route = (path: string, view: http.RequestListener, method = 'use') => {
+    return new Route(path, view, method);
 }
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import Route from './route';
+import Server from './server';
 
 export default class Router {
     private _routes: Array<Route>;
@@ -10,4 +11,9 @@ export default class Router {
     get routes(){
         return this._routes;
     }
+
+    public assignServer(server: Server){
+        this._routes.forEach(route => route.assignInServer(server))
+    }
+
 }
