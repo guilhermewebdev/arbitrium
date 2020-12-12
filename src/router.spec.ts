ce3ddfda0b81c02830b70f618bdf33faccba2c28
Guilherme Isaías <guilherme@localhost.localdomain>
2020-12-12 15:27:42 -0300

    Criação do teste da instancia do Router

---
+++ src/router.spec.ts
@@ -0,0 +1,9 @@
+const Router = require('./router');
+
+describe('Test the router class', () => {
+    test('Instance router class', () => {
+        const router = Router([
+
+        ])
+    })
+})
