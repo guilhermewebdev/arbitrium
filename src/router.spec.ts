7c3a27cd53522543893b2ddfb3d16039838387c0
Guilherme Isaías <guilherme@localhost.localdomain>
2020-12-12 15:30:36 -0300

    Teste da rota

--- src/router.spec.ts
+++ src/router.spec.ts
@@ -1,9 +1,10 @@
 const Router = require('./router');
+const Route = require('./route');
 
 describe('Test the router class', () => {
     test('Instance router class', () => {
         const router = Router([
-
+            Route('/', console.log)
         ])
     })
 })
