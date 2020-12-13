4fa7cbdf3fd6b1e84fca121ebb61bb0e41c43b7a
Guilherme Isaías <guilherme@localhost.localdomain>
2020-12-13 19:25:32 -0300

    Teste na rota

--- src/route.spec.ts
+++ src/route.spec.ts
@@ -1,15 +1,13 @@
 import { ServerRequest } from "https://deno.land/std@0.80.0/http/server.ts";
 import Route from './route.ts';
-import {
-    assertArrayIncludes,
-    assertEquals,
-  } from "https://deno.land/std@0.80.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.80.0/testing/asserts.ts";
 
 const view = (request: ServerRequest, args: any) => {
     return;
 }
 
 Deno.test('Test route', () => {
-    const route = new Route('/', view);
-    assertEquals(route.match(''), true)
+    const root = new Route('/', view);
+    assertEquals(root.match(''), false)
+    assertEquals(root.match('/'), true)
 })
